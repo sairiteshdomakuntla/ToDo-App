3d1c6f688928320e6d0add3abf4723412e12a6f5
sairiteshdomakuntla <sairiteshdomakuntla@gmail.com>
2025-02-16 18:51:32 +0530

    v3

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-
+const path=require("path")
 // Middleware
 app.use(cors());
 app.use(express.json());
@@ -19,6 +19,16 @@ app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/todos', require('./routes/todoRoutes'));
 
 const PORT = process.env.PORT || 5000;
+console.log("Serving static files from:", path.join(__dirname, "client", "dist"));
+
+app.use(express.static(path.join(__dirname,"client","dist")));
+
+app.get("*",(req,res)=>{
+  res.sendFile(path.join(__dirname,"./client/dist/index.html",))
+})
+
+
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
